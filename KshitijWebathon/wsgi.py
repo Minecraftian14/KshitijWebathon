b3642ba9ee2c84e6481455e6760eb3001d8ccca9
Minecraftian14 <mcxiv14@gmail.com>
2023-01-28 18:49:23 +0530

    Update wsgi.py

--- KshitijWebathon/wsgi.py
+++ KshitijWebathon/wsgi.py
@@ -1,25 +1,18 @@
-"""
-WSGI config for KshitijWebathon project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
 import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "KshitijWebathon.settings")
+django.setup()
 
 from django.core.wsgi import get_wsgi_application
-
 from . import mySecrets
 from smartathon.views import sio
 
 from socketio import WSGIApp
-
 import eventlet
 import eventlet.wsgi
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "KshitijWebathon.settings")
+print("Just a random number: ", 14)
 
 if mySecrets.useSimpleTech:
     application = get_wsgi_application()
